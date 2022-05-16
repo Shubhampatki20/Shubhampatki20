d38961143993f7e746f0c70dd2f23bf2d088eed1
Shubhampatki20 <104133825+Shubhampatki20@users.noreply.github.com>
2022-05-16 11:18:26 +0530

    Self PC
    Python programs from Basic to Advance > Sum of two integer numbers.

---
+++ main.py
@@ -0,0 +1,8 @@
+# Simple calculator which takes input from user and add them each other "Only sum"
+
+print("Enter number 1 :")
+Number1 = input()
+
+print("Enter number 1:")
+Number2 = input()
+print("Sum of given 2 numbers is",int(Number1)+int(Number2))
